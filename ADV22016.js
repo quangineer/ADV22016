cdb90c20c590d78badf238aca70243a4a2f28bf3
quanigneer <quanigneer@gmail.com>
2022-06-09 11:52:00 -0700

    update

---
+++ ADV22016.js
@@ -0,0 +1,13 @@
+document.getElementById('inputfile').addEventListener('change', function () {
+    let fr = new FileReader();
+    fr.onload = function () {
+        // document.getElementById('output').innerHTML = fr.result;
+        solution(fr.result);
+    }
+
+    fr.readAsText(this.files[0]);
+})
+
+function solution(text) {
+    
+}
