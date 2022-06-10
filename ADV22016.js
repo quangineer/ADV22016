0c0dc56425fe681a01f18e37f815b083f98fe776
quanigneer <quanigneer@gmail.com>
2022-06-10 15:02:45 -0700

    update

--- ADV22016.js
+++ ADV22016.js
@@ -9,15 +9,15 @@ document.getElementById('inputfile').addEventListener('change', function () {
 })
 
 function solution(text) {
-    splittext = text.split("\n");
-    newtext = splittext.filter(element => {
+    splittext = text.split("\n");                       //kill all space line
+    newtext = splittext.filter(element => {             //kill the empty element 
         return element !== "";
     });
     arraytext = []
     for (i=0;i<newtext.length;i++){
-        arraytext.push(newtext[i].split(""));
+        arraytext.push(newtext[i].split(""));           //split all element in each nested array
     }
-
+    console.log(arraytext);
     let keypad = [[1,2,3],[4,5,6],[7,8,9]];
 
     
